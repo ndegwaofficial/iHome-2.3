2e69ae6b81c3ffddc4298cdc57dcf352198b6994
Ndegwa Official <ndegwaofficial@pop-os.localdomain>
2021-01-20 15:06:12 +0300

    Setup the GraphQL Typescript Server side code in Packages/server folder

---
+++ packages/server/src/types/graphql-utils.ts
@@ -0,0 +1,34 @@
+import { Redis } from "ioredis";
+
+export interface Session extends Express.Session {
+  destroy(arg0: (err: any) => void);
+  userId?: string;
+}
+
+export interface Context {
+  redis: Redis;
+  url: string;
+  session: Session;
+  req: Express.Request;
+}
+
+export type Resolver = (
+  parent: any,
+  args: any,
+  context: Context,
+  info: any
+) => any;
+
+export type GraphQLMiddlewareFunc = (
+  resolver: Resolver,
+  parent: any,
+  args: any,
+  context: Context,
+  info: any
+) => any;
+
+export interface ResolverMap {
+  [key: string]: {
+    [key: string]: Resolver;
+  };
+}
